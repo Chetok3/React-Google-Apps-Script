7dbbcb71a90350fa94b3e97e40e6624e8649b37a
Elisha Nuchi <31550519+enuchi@users.noreply.github.com>
2025-04-03 12:26:49 -0400

    update jest

--- test/puppeteer-environment.js
+++ test/puppeteer-environment.js
@@ -10,8 +10,9 @@ import NodeEnvironment from 'jest-environment-node';
 
 const DIR = path.join(os.tmpdir(), 'jest_puppeteer_global_setup');
 
-export default class PuppeteerEnvironment extends NodeEnvironment.TestEnvironment {
+export default class PuppeteerEnvironment extends NodeEnvironment.default {
   async setup() {
+    await super.setup();
     // get the wsEndpoint
     const wsEndpoint = await readFile(path.join(DIR, 'wsEndpoint'), 'utf8');
     if (!wsEndpoint) {
@@ -22,5 +23,13 @@ export default class PuppeteerEnvironment extends NodeEnvironment.TestEnvironmen
     this.global.__BROWSER_GLOBAL__ = await puppeteer.connect({
       browserWSEndpoint: wsEndpoint,
     });
+  }
+
+  async teardown() {
+    await super.teardown();
+  }
+
+  getVmContext() {
+    return super.getVmContext();
   }
 }
